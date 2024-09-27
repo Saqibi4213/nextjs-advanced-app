92cdee2cfc6c3917db5e4a1aee81a7f5c75d2f87
Saqibi4213 <mehriamosawer@gmail.com>
2024-09-27 23:43:34 +0430

    thilwand css added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [   './src/**/*.{js,ts,jsx,tsx}',],
+  content: [    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        chocolate: {
+          800: '#4B3D3C',
+          600: '#7B5B54',
+          700: '#4E3B31',
+        },
+        brown: {
+          700: '#4E3B31',
+        },
+      },
+    },
   },
   plugins: [],
 }
